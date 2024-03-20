06ad7f8f625552041c348fa98cea41bfe3c8e0de
Wesley Verweij <wesleyverweij2004@gmail.com>
2024-03-20 22:29:29 +0100

    fix issue with database models

--- api/database/session.py
+++ api/database/session.py
@@ -7,12 +7,12 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine(
     settings.DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 def get_db() -> Generator:
-    db = SessionLocal()
     try:
+        db = SessionLocal()
         yield db
     finally:
         db.close()
