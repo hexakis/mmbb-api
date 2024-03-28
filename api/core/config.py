0f113fb6925b28b130455d02f7f86e95b7258282
Wesley Verweij <wesleyverweij2004@gmail.com>
2024-03-29 00:07:06 +0100

    fixed networking issues

--- api/core/config.py
+++ api/core/config.py
@@ -4,10 +4,18 @@ from pathlib import Path
 
 
 class Settings:
-    PROJECT_NAME: str = os.getenv("PROJECT_NAME")
-    PROJECT_VERSION: str = os.getenv("PROJECT_VERSION")
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    NOOB_TOKEN: str = os.getenv("NOOB_TOKEN")
+    if os.getenv("PROJECT_STATUS") == "PROD":
+        PROJECT_NAME: str = os.getenv("PROJECT_NAME")
+        PROJECT_VERSION: str = os.getenv("PROJECT_VERSION")
+        DATABASE_URL: str = os.getenv("DATABASE_URL")
+        NOOB_TOKEN: str = os.getenv("NOOB_TOKEN")
+        HASHING_SECRET: str = os.getenv("HASH_SECRET")
+    elif os.getenv("PROJECT_STATUS") == "DEV":
+        PROJECT_NAME: str = os.getenv("DEV_NAME")
+        PROJECT_VERSION: str = os.getenv("DEV_VERSION")
+        DATABASE_URL: str = os.getenv("DEV_DATABASE_URL")
+        NOOB_TOKEN: str = os.getenv("NOOB_TOKEN")
+        HASHING_SECRET: str = os.getenv("HASH_SECRET")
 
 
 settings = Settings()
